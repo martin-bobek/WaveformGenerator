80f4e762bc99fddda2de506116e6198f3a5c4e86
martin-bobek <martinbobek77@hotmail.com>
2017-10-27 21:13:31 -0600

    Initial commit

---
+++ sim_1/new/tb_waveform_generator.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tb_waveform_generator is end;
+
+architecture Behavioral of tb_waveform_generator is
+    component waveform_generator is 
+    end component;
+begin
+
+
+end Behavioral;
---
+++ sources_1/new/waveform_generator.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity waveform_generator is 
+
+end;
+
+architecture behavioural of waveform_generator is
+
+begin
+
+end;
