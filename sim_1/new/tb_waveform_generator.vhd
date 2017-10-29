5f13c5724a5f72aee01eee8d9c37339f74a71ab8
martin-bobek <martinbobek77@hotmail.com>
2017-10-29 12:10:28 -0600

    Configured ports

--- sim_1/new/tb_waveform_generator.vhd
+++ sim_1/new/tb_waveform_generator.vhd
@@ -5,6 +5,11 @@ entity tb_waveform_generator is end;
 
 architecture Behavioral of tb_waveform_generator is
     component waveform_generator is 
+        port(
+            clk: in std_logic;
+            pwm: out std_logic;
+            buttons: in std_logic_vector(4 downto 0)
+        );
     end component;
 begin
 
--- sources_1/new/waveform_generator.vhd
+++ sources_1/new/waveform_generator.vhd
@@ -2,7 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity waveform_generator is 
-
+    port(
+        clk: in std_logic;
+        pwm: out std_logic;
+        buttons: in std_logic_vector(4 downto 0)
+    );
 end;
 
 architecture behavioural of waveform_generator is
