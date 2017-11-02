c30c55a19221fe74b85a8d53bd6fc56debc5f8c5
Martin Bobek <martin.bobek@ucalgary.ca>
2017-11-02 15:03:24 -0600

    Scales

--- sources_1/new/triangle.vhd
+++ sources_1/new/triangle.vhd
@@ -28,12 +28,21 @@ architecture behavioural of triangle is
     constant period_full: unsigned(7 downto 0) := to_unsigned(199, 8);
 
     signal captured_period: std_logic_vector(12 downto 0);
-    signal tick_counter: unsigned(7 downto 0);
+    signal captured_amplitude: unsigned(6 downto 0);
+    signal tick_counter, tick_level: unsigned(7 downto 0);
     signal tick: std_logic;
     signal u_value: unsigned(6 downto 0) := (others => '0');
+    
+    signal level_prod, tick_prod: unsigned(14 downto 0);
 begin
     value <= std_logic_vector(u_value);
+    
+    tick_level <= tick_counter when (tick_counter <= period_half) else
+                 (period_full - tick_counter);
 
+    level_prod <= resize(to_unsigned(100, 7) * u_value, 15);
+    tick_prod <= resize(captured_amplitude * tick_level, 15);
+    
     tick_gen: tick_generator
         port map(
             clk => clk,
@@ -45,20 +54,31 @@ begin
     
     process(clk, reset) begin
         if (reset = '1') then
-            u_value <= "0000010";
+            u_value <= (others => '0');
             tick_counter <= period_full;
             captured_period <= (others => '0');
+            captured_amplitude <= (others => '0');
         elsif rising_edge(clk) and (tick = '1') then
             if (tick_counter = period_full) then
                 tick_counter <= (others => '0');
                 u_value <= (others => '0');
                 captured_period <= tick_period;
-            elsif (tick_counter <= period_half) then
-                u_value <= u_value + 1;
+                captured_amplitude <= unsigned(amplitude);
+            elsif (tick_counter = period_half) then
+                u_value <= captured_amplitude;
                 tick_counter <= tick_counter + 1;
+            elsif (tick_counter < period_half) then
+                tick_counter <= tick_counter + 1;
+                
+                if (tick_prod >= level_prod) then
+                    u_value <= u_value + 1;
+                end if;
             else
                 tick_counter <= tick_counter + 1;
-                u_value <= u_value - 1;
+                
+                if (tick_prod + 1 <= level_prod) then
+                    u_value <= u_value - 1;
+                end if;
             end if;
         end if;
     end process;
